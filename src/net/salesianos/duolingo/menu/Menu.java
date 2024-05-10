21082eea15478c86afb759d1a02d0d03ef6a7be2
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-05-10 13:09:32 +0100

    feat: mostrar menú

--- src/net/salesianos/duolingo/menu/Menu.java
+++ src/net/salesianos/duolingo/menu/Menu.java
@@ -1,5 +1,17 @@
 package net.salesianos.duolingo.menu;
 
 public class Menu {
-  
+    public final static void mostrarMenu() {
+        String menu = "\n--------------------------------------=== <Diccionario DUOLINGO> ===----------------------------------"
+                +
+                "\n* Pulsa: <a> para añadir una nueva palabra." +
+                "\n* Pulsa: <b> para eliminar una palabra." +
+                "\n* Pulsa: <c> para buscar una palabra." +
+                "\n* Pulsa: <d> para mostrar las iniciales disponibles." +
+                "\n* Pulsa: <e> para ver las palabras ordenadas por las iniciales." +
+                "\n* Pulsa: <s> si deseas salir del programa."
+                +
+                "\n--------------------------------------=== <Diccionario DUOLINGO> ===----------------------------------";
+        System.out.println(menu);
+    }
 }
